7a3699d3247468ca1377e7e97968f20459fe0365
GeorgiSotirovv <tirko123456@abv.bg>
2022-11-09 20:07:33 +0200

    Add project files.

---
+++ CigarWorld/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using CigarWorld.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CigarWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("ShowCigar", "Cigar");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ CigarWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CigarWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
