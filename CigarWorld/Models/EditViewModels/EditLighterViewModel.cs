98964975b9cd26bf9cf49269621e2dce963d9b81
GeorgiSotirovv <tirko123456@abv.bg>
2022-12-02 13:25:34 +0200

    Make some changes and fix bugs

---
+++ CigarWorld/Models/EditViewModels/EditLighterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CigarWorld.Models.EditViewModels
+{
+    public class EditLighterViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Brand { get; set; } = null!;
+
+        [Required]
+        public string CountryOfManufacturing { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        public string Comment { get; set; } = null!;
+    }
+}
